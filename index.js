a551f10d28cab83388ca57b32b7336f387c4d691
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-01-11 01:09:20 +0100

    middleware checkAuth + new get request

--- index.js
+++ index.js
@@ -18,11 +18,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/auth/me", checkAuth, (req, res) => {
+app.get("/auth/me", checkAuth, async (req, res) => {
   try {
-    res.json({
-      success: true,
-    });
+    const user = await UserModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User is undefined",
+      });
+    }
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
   } catch (error) {}
 });
 
